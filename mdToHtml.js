b63984fb372c3dd084c416b34d6e59bc3cde3b52
Fede-Rausa <126892947+Fede-Rausa@users.noreply.github.com>
2024-10-22 22:37:24 +0200

    Update mdToHtml.js

--- mdToHtml.js
+++ mdToHtml.js
@@ -5,6 +5,7 @@ function loadMarkdown(path) {
             const converter = new showdown.Converter();   // Initialize Showdown converter
             const html = converter.makeHtml(markdown);    // Convert Markdown to HTML
             document.getElementById('target').innerHTML = html;    // Get a reference to the target element and add the HTML content
+            console.log("conversion applied");
         })
         .catch(error => {
             document.getElementById('target').innerHTML = '<p>Error loading the content.</p>';
